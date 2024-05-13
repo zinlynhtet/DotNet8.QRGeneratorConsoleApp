2731c521dc104acc99801953d21d7cb9685bb5a1
Zin Linn Htet <mackk5504@gmail.com>
2024-05-13 13:34:10 +0630

    Complete Qr Generator

--- DotNet.QRGeneratorConsoleApp/Program.cs
+++ DotNet.QRGeneratorConsoleApp/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using QRCoder;
+
+Console.WriteLine("Enter the data to encode in the QR code:");
+var data = Console.ReadLine()!;
+var  qrGenerator = new QRCodeGenerator();
+var qrCodeData = qrGenerator.CreateQrCode(data, QRCodeGenerator.ECCLevel.Q);
+var qrCode = new QRCode(qrCodeData);
+Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+var folderPath = @"C:\Users\hlain\Downloads";
+if (!Directory.Exists(folderPath))
+{
+    Directory.CreateDirectory(folderPath);
+}
+
+string fileName = Path.Combine(folderPath, data + "_" + "QRCode.png");
+qrCodeImage.Save(fileName, ImageFormat.Png);
+
+DisplayQRCodeImage(fileName);
+
+Console.ReadLine();
+
+static void DisplayQRCodeImage(string imagePath)
+{
+    try
+    {
+        if (File.Exists(imagePath))
+        {
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = imagePath,
+                UseShellExecute = true
+            };
+            Process.Start(psi);
+        }
+        else
+        {
+            Console.WriteLine("QR code image not found.");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+}
